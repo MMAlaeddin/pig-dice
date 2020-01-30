647af116718acb11c75a10d4592b56970490cc22
Student <example@example.com>
2020-01-30 09:29:29 -0800

    added constructors and prototypes to push scoreboard and start game/ add player

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,67 @@
-function Player(currentScore, finalScore) {
-  this.currentScore = currentScore,
-  this.finalScore += clickRoll
+// Game Business Logic
+
+function Game() {
+  this.players = []
+  this.currentId = 0
+}
+ 
+Game.prototype.assignId = function () {
+  this.currentId += 1; 
+  return this.currentId;
 }
 
-var finalScore = [];
-var sum = 0;
-// for (var i = 0; i <= finalScore.length; i ++) {
-//   finalScore += clickRoll;
+Game.prototype.addPlayer = function (player) {
+  player.id = this.assignId();
+  this.players.push(player);
+}
 
+// Player Business Logic
 
+function Player(gameScore, rollScore) {
+  this.gameScore = gameScore,
+  this.rollScore = rollScore
+}
 
-//front end
+// UI logic
 
+var game = new Game(); 
+
+var diceRoll = function() {
+  var clickRoll = Math.floor(
+  Math.random() * 6 ) +1;
+  return clickRoll;
+  }
+
+function endTurn() {
+  if (gameScore >= 100) {
+    alert("WINNER!")
+  }
+
+}
+  Player.prototype.rollOutcome = function() {
+    var rolling = diceRoll(); 
+      if (diceRoll != 1) {
+        this.gameScore += rolling;
+      } else { 
+        (diceRoll == 1) 
+        this.gameScore += 0;
+      } 
+      
+      // $("#p1Score").text(gameScore);
+   }
 
 $(document).ready(function() {
   $("#button1").click(function(event) {
     event.preventDefault();
 
-    var clickRoll = Math.floor(
-      Math.random() * 6 ) +1;
       parseInt($(".rollOutput").text(clickRoll));
       finalScore.push(clickRoll);
       $("#p1Score").append("<li>" + clickRoll + "</li>");
-   
-
-   
-
   })
   
   $("#button2").click(function(event) {
     event.preventDefault();
 
-    var clickRoll = Math.floor(
-      Math.random() * 6 ) +1;
       parseInt($(".rollOutput").text(clickRoll));
       finalScore.push(clickRoll);
       $("#p2Score").append("<li>" + clickRoll + "</li>");
